14baab840f5093857577eb51903fd47b3dd8a6a5
Paolo Bosetti <paolo.bosetti@unitn.it>
2020-11-17 18:34:32 +0100

    Commented polyline.c

--- examples/src/polyline.c
+++ examples/src/polyline.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+// a segment, intended as a target position, being the start position that 
+// of the previous segment
 typedef struct {
   struct {
     double x, y;
@@ -9,27 +11,43 @@ typedef struct {
   void *prev;
 } segment_t;
 
+
+// a polyline, intended as a linked list of segments
+// by convention, the coordinates of the first segment are the starting
+// point of the polyline
 typedef struct {
   segment_t *first;
   segment_t *last;
 } poly_t;
 
+// create a new segment that goes from the previous position
+// to the point of coordinates x, y
 segment_t *segment_new(double x, double y) {
   /* to be completed */
 }
 
+// create a new polyline, starting at coordinates x, y
 poly_t *poly_new(double x, double y) {
   /* to be completed */
 }
 
+// add the segment seg to the existing polyline poly
 void poly_add(poly_t *poly, segment_t *seg) {
   /* to be completed */
 }
 
+// insert a new segment seg after the existing segment at index pos
+// (being 0 the first segment)
 void poly_insert_at(poly_t *poly, segment_t *seg, size_t pos) {
   /* to be completed */
 }
 
+// print a polyline description
+void poly_print(poly_t *poly) {
+  /* to be completed */
+}
+
+// free memory allocated for poly
 void poly_free(poly_t *poly) {
   /* to be completed */
 }
@@ -46,7 +64,7 @@ int main(int argc, char const *argv[]) {
   }
 
   // forward
-  /* to be completed: loop and print vertex positions and index from first to last 
+  /* to be completed: print vertex positions and index from first to last 
     format must be:
     1: {1, 6}
     2: {3, 2}
@@ -54,7 +72,7 @@ int main(int argc, char const *argv[]) {
   */
 
   // Backward
-  /* to be completed: loop and print vertex positions and index from last to first
+  /* to be completed: print vertex positions and index from last to first
     format must be:
     10: {1, 7}
      9: {5, 5}
