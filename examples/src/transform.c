bfabf6eccdd1c03597568016e2c9eac2e1d8d16f
Paolo Bosetti <paolo.bosetti@unitn.it>
2022-03-31 13:28:29 +0200

    Improved transform.c example

--- examples/src/transform.c
+++ examples/src/transform.c
@@ -1,70 +1,80 @@
+//   _____                     __                      
+//  |_   _| __ __ _ _ __  ___ / _| ___  _ __ _ __ ___  
+//    | || '__/ _` | '_ \/ __| |_ / _ \| '__| '_ ` _ \
+//    | || | | (_| | | | \__ \  _| (_) | |  | | | | | |
+//    |_||_|  \__,_|_| |_|___/_|  \___/|_|  |_| |_| |_|
+// 
+// Roto-translation of a point in 3D
+// Rotation is about Z axis on XY plane
+// Build with:
+//    clang -lgsl -lgslcblas -lm -o transform src/transform.c
 #include <gsl/gsl_matrix.h>
 #include <gsl/gsl_vector.h>
 #include <gsl/gsl_blas.h>
-
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-
 #include <math.h>
 
+// point object that we are going to use in C-CNC
 typedef struct {
-  double x,y,z;
+  double x, y, z;
 } point_t;
 
-int transform(point_t *pt, point_t *d, double angle) {
-  int rv;
-  gsl_matrix *T = gsl_matrix_alloc(4,4);
+// transform point with angle rotation followed by delta translation
+// the transformed coordinate is in point
+int transform(point_t *point, double angle, point_t *delta) {
+  gsl_matrix *T = gsl_matrix_alloc(4, 4);
   gsl_vector *p = gsl_vector_alloc(4);
   gsl_vector *p1 = gsl_vector_alloc(4);
+  int rv;
 
   double th = angle / 180.0 * M_PI;
-  double sin_th = sin(th);
-  double cos_th = cos(th);
+  double sa = sin(th);
+  double ca = cos(th);
 
-  // Fill gsl_vector p with point pt
   gsl_vector_set_all(p, 1);
-  gsl_vector_set(p, 0, pt->x);
-  gsl_vector_set(p, 1, pt->y);
-  gsl_vector_set(p, 2, pt->z);
+  gsl_vector_set(p, 0, point->x);
+  gsl_vector_set(p, 1, point->y);
+  gsl_vector_set(p, 2, point->z);
 
   gsl_matrix_set_identity(T);
-  gsl_matrix_set(T, 0, 0, cos_th);
-  gsl_matrix_set(T, 0, 1, -sin_th);
-  gsl_matrix_set(T, 0, 3, d->x);
-  // Second line
-  gsl_matrix_set(T, 1, 0, sin_th);
-  gsl_matrix_set(T, 1, 1, cos_th);
-  gsl_matrix_set(T, 1, 3, d->y);
-  // Third line
-  gsl_matrix_set(T, 2, 3, d->z);
+  // first row
+  gsl_matrix_set(T, 0, 0, ca);
+  gsl_matrix_set(T, 0, 1, -sa);
+  gsl_matrix_set(T, 0, 3, delta->x);
+  // second row
+  gsl_matrix_set(T, 1, 0, sa);
+  gsl_matrix_set(T, 1, 1, ca);
+  gsl_matrix_set(T, 1, 3, delta->y);
+  // third row
+  gsl_matrix_set(T, 2, 3, delta->z);
 
+  // BLAS level 2: GEneral Matrix-Vector product (Double)
   rv = gsl_blas_dgemv(CblasNoTrans, 1, T, p, 0, p1);
 
-  pt->x = gsl_vector_get(p1, 0);
-  pt->y = gsl_vector_get(p1, 1);
-  pt->z = gsl_vector_get(p1, 2);
+  point->x = gsl_vector_get(p1, 0);
+  point->y = gsl_vector_get(p1, 1);
+  point->z = gsl_vector_get(p1, 2);
 
   gsl_vector_free(p);
   gsl_vector_free(p1);
   gsl_matrix_free(T);
-
   return rv;
-
 }
 
 
-int main() {
-  // Input point
-  point_t pt = {.x=10, .y=3, .z=7};
+int main(int argc, char const *argv[]) {
+  point_t pt = {.x=10, .y=0, .z=0};
+  point_t d = {.x=10, .y=10, .z=5};
+  double alpha = 45.0;
 
-  point_t d = {.x=10, .y=0, .z=5}; // Traslate
-  double angle = 45.0f; // Rotate
+  printf("Roto-translation transform in XY plane\n");
+  printf("Before:      (%8.4f, %8.4f, %8.4f)\n", pt.x, pt.y, pt.z);
+  printf("Translation: (%8.4f, %8.4f, %8.4f)\n", d.x, d.y, d.z);
+  printf("Rotation:    %+8.3f°\n", alpha);
+  transform(&pt, alpha, &d);
+  printf("After:       (%8.4f, %8.4f, %8.4f)\n", pt.x, pt.y, pt.z);
 
-  fprintf(stdout, "pt = (%6.3f - %6.3f - %6.3f)\n", pt.x, pt.y, pt.z);
-  fprintf(stdout, "d  = (%6.3f - %6.3f - %6.3f)\n", d.x, d.y, d.z);
-  fprintf(stdout, "angle = %6.3f\n", angle);
-  int rv = transform(&pt, &d, angle);
-  fprintf(stdout, "rotated = (%6.3f - %6.3f - %6.3f)\n", pt.x, pt.y, pt.z);
-
+  return 0;
 }
